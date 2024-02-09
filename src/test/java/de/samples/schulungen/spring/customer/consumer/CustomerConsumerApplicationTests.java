1c097fd6cb3e99a4a82e2c5f8d6c6052243a6422
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-02-09 15:09:31 +0100

    Initial commit

---
+++ src/test/java/de/samples/schulungen/spring/customer/consumer/CustomerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.samples.schulungen.spring.customer.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
